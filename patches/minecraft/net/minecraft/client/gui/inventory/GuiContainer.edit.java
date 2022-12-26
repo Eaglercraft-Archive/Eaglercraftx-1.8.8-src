6041bfbbd7472828e4892d8d626f59927a3e3163
LAX1DUDE <cgiacun@gmail.com>
2022-12-26 01:50:25 -0800

    Update #3 - Fixed graphical bug in survival inventory

--- patches/minecraft/net/minecraft/client/gui/inventory/GuiContainer.edit.java
+++ patches/minecraft/net/minecraft/client/gui/inventory/GuiContainer.edit.java
@@ -24,27 +24,31 @@
 
 > DELETE  22  @  19 : 20
 
-> CHANGE  210 : 211  @  208 : 209
+> INSERT  103 : 104  @  101
+
++ 			GlStateManager.enableAlpha();
+
+> CHANGE  211 : 212  @  208 : 209
 
 ~ 				EaglerTextureAtlasSprite textureatlassprite = this.mc.getTextureMapBlocks().getAtlasSprite(s1);
 
-> CHANGE  267 : 268  @  265 : 266
+> CHANGE  268 : 269  @  265 : 266
 
 ~ 	protected void mouseClicked(int parInt1, int parInt2, int parInt3) {
 
-> CHANGE  503 : 507  @  501 : 503
+> CHANGE  504 : 508  @  501 : 503
 
 ~ 	protected void keyTyped(char parChar1, int parInt1) {
 ~ 		if (parInt1 == this.mc.gameSettings.keyBindClose.getKeyCode()
 ~ 				|| parInt1 == this.mc.gameSettings.keyBindInventory.getKeyCode()
 ~ 				|| (parInt1 == 1 && this.mc.gameSettings.keyBindClose.getKeyCode() == 0)) {
 
-> CHANGE  508 : 510  @  504 : 512
+> CHANGE  509 : 511  @  504 : 512
 
 ~ 			if (this.mc.currentScreen == null) {
 ~ 				this.mc.setIngameFocus();
 
-> INSERT  511 : 522  @  513
+> INSERT  512 : 523  @  513
 
 + 		} else if (parInt1 == 1) {
 + 			showingCloseKey = System.currentTimeMillis();
@@ -58,6 +62,6 @@
 + 				}
 + 			}
 
-> DELETE  523  @  514 : 515
+> DELETE  524  @  514 : 515
 
 > EOF
