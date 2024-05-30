322e653ca9142d3be84bfc61e366bea26e3a0338
lax1dude <cgiacun@gmail.com>
2024-05-29 20:01:31 -0700

    <1.0.1> Fix rare memleak in EaglerXVelocity's HttpWebServer

--- gateway/EaglercraftXVelocity/src/main/java/net/lax1dude/eaglercraft/v1_8/plugin/gateway_velocity/EaglerXVelocityVersion.java
+++ gateway/EaglercraftXVelocity/src/main/java/net/lax1dude/eaglercraft/v1_8/plugin/gateway_velocity/EaglerXVelocityVersion.java
@@ -17,13 +17,13 @@ package net.lax1dude.eaglercraft.v1_8.plugin.gateway_velocity;
  */
 public class EaglerXVelocityVersion {
 
-	public static final String NATIVE_VELOCITY_BUILD = "3.3.0-SNAPSHOT:afd8b55:b390";
+	public static final String NATIVE_VELOCITY_BUILD = "3.3.0-SNAPSHOT:1c36b66d:b394";
 
 	public static final String ID = "EaglerXVelocity";
 	public static final String PLUGIN_ID = "eaglerxvelocity";
 	public static final String NAME = "EaglercraftXVelocity";
 	public static final String DESCRIPTION = "Plugin to allow EaglercraftX 1.8 players to join your network, or allow EaglercraftX 1.8 players to use your network as a proxy to join other networks";
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "1.0.1";
 	public static final String[] AUTHORS = new String[] { "lax1dude", "ayunami2000" };
 
 }
